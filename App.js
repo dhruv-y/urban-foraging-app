89e5d54dc03abec756ab9260d98ca146effebdbb
Dhruv Yadwadkar <dhruvyadwadkar@gmail.com>
2020-10-06 23:56:35 -0400

    Updated Packages + Set up skeleton

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome!</Text>
+      <Text>Urban Forager</Text>
       <StatusBar style="auto" />
     </View>
   );
