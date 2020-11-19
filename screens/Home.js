acc3461dd7ea3584df565d48ff2ddd4ae0ef4f9a
Dhruv Yadwadkar <dhruvyadwadkar@gmail.com>
2020-11-18 23:31:07 -0500

    Home Page Updated

--- screens/Home.js
+++ screens/Home.js
@@ -12,27 +12,25 @@ import Block from "../components/Block";
 import Button from "../components/Button";
 import Text from "../components/Text";
 import { theme } from "../constants";
-
+import { firebase } from '../firebase/config'
 const { width, height } = Dimensions.get("window");
 
 class Home extends React.Component {
 
+    componentDidMount() {
+        firebase.auth().signInWithEmailAndPassword('dyadwad@iu.edu', 'password');
+    }
+
     render() {
         const { navigation } = this.props;
-
         return (
             <Block center middle>
                 <Block center middle flex={0.5}>
-                    <Text h1 center bold>
-                        Urban Foraging. <Text h1 primary>Simplfied.</Text>
-                        <Text h1 primary>
-                            {" "}
-                            Simpler.
-                        </Text>
+                    <Text h1 center bold primary>
+                        Forage.
                     </Text>
-
                     <Text h3 gray2 style={{ marginTop: theme.sizes.padding / 2 }}>
-                        Enjoy the experience.
+                        Urban Foraging. Simplified.
                     </Text>
                 </Block>
                 <Block center middle>
@@ -44,15 +42,20 @@ class Home extends React.Component {
                 </Block>
 
                 <Block middle flex={0.5} margin={[0, theme.sizes.padding * 2]}>
-                    <Button gradient onPress={() => navigation.navigate("Explore")}>
+                    <Button gradient onPress={() => navigation.navigate("Favorites")}>
                         <Text center semibold white>
-                            Explore
+                            Favorites
                         </Text>
                     </Button>
 
                     <Button shadow onPress={() => navigation.navigate("Map")}>
                         <Text center semibold>
                             Edible Tree Map
+                        </Text>
+                    </Button>
+                    <Button shadow onPress={() => navigation.navigate("Login")}>
+                        <Text center semibold>
+                            Login
                         </Text>
                     </Button>
                 </Block>
